13eae3f2105762d16e6a0e6086fa6ab2ec8b7eb4
Carter Hunt Fogelman <chfogelman@gmail.com>
2024-10-02 23:17:42 -0500

    Clean up neovim config

--- .config/nvim/lua/lsp-configs.lua
+++ .config/nvim/lua/lsp-configs.lua
@@ -31,30 +31,22 @@ lspconfig.rust_analyzer.setup {
 }
 
 lspconfig.lua_ls.setup {
-    on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                Lua = {
-                    runtime = {
-                        version = 'LuaJIT'
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        }
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    }
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT'
+            },
+            diagnostics = {
+                globals = {
+                    'vim',
+                    'require'
                 }
-            })
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        end
-        return true
-    end
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true)
+            }
+        }
+    }
 }
 
 lspconfig.clangd.setup {}
